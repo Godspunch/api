f2a150cda194041d70b04b66fe02a46dfae5047f
Gpunch <koray.civelek98@gmial.com>
2024-02-09 00:43:54 +0300

    First view applied to project

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-</head>
-<body>
-    <?php
-        require("model.php");
-        $products = new Model;
-        $allproducts = $products->getData();
-    ?>
-    <h1>Ürünler</h1>
-    <?php foreach($allproducts as $product): ?>
-    <h2><?= htmlspecialchars($product["title"]); ?></h2>
-    <p><?= htmlspecialchars($product["description"]); ?> </p>
-    <?php endforeach; ?>
-</body>
-</html>
+<?php
+    require("model.php");
+    $products = new Model;
+    $allproducts = $products->getData();
+
+    require("view.php");
+
---
+++ view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Ürünler</h1>
+    <?php foreach($allproducts as $product): ?>
+    <h2><?= htmlspecialchars($product["title"]); ?></h2>
+    <p><?= htmlspecialchars($product["description"]); ?> </p>
+    <?php endforeach; ?>
+</body>
+</html>
